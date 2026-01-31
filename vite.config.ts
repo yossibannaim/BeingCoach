f6d7a28d718e811209f97511338b3c58f5aacdfc
yossibannaim <yossibannaim@gmail.com>
2026-01-31 18:04:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +6,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // הוספת השורה הזו מבטיחה שהנתיבים יהיו יחסיים לשם הריפוזיטורי
+      base: '/BeingCoach/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
